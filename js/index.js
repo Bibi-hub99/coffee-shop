7983736a650eee7fa178535afc882ca04b4f9864
Bibi-hub99 <anxumalo000@gmail.com>
2025-04-21 23:15:02 +0200

    fully functional homepage build successfully

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ mobileShowSlideBtn.addEventListener("click",showMobileSlide)
 const closeMobileSlideBtn = document.getElementById("close-mobile-slide")
 closeMobileSlideBtn.addEventListener("click",hideMobileSlide)
 
+const joinNowBtn = document.getElementById('join-now-btn')
+joinNowBtn.addEventListener("click",navigateToMembership)
+
 function showMobileSlide(){
     document.getElementById("mobile-slide").style.width = '50%'
 }
@@ -10,3 +13,21 @@ function showMobileSlide(){
 function hideMobileSlide(){
     document.getElementById("mobile-slide").style.width = '0%'
 }
+
+function triggerCookiesBanner(){
+
+    const cookiesExist = JSON.parse(localStorage.getItem("cookie-state"))
+
+    if(!cookiesExist && cookiesExist !== false && cookiesExist !== true){
+        console.log(cookiesExist)
+        document.getElementById("cookies-banner").style.display = 'block'
+    }else{
+        document.getElementById("cookies-banner").style.display = 'none'
+    }
+}
+
+function navigateToMembership(){
+    window.location.href = './coffee/membership.html'
+}
+
+setTimeout(triggerCookiesBanner,3000)
